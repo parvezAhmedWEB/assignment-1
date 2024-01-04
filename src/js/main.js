d07d9fcd64e9edcbf9d01017d511820277937393
Parvez Ahmed Prottoy <parvezahmedprottoy.web@gmail.com>
2024-01-04 18:42:26 +0600

    header section complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log("hello...");
+/* ===================== CREATE NAV ITEM START ===================== */
+const navContainer = document.querySelector(".navbar-nav");
+const navItems = [
+  "Home",
+  "Hotels",
+  "Team",
+  "Blog",
+  "Pricing",
+  "FAQs",
+  "Contact",
+];
+function createNavItem(navItems) {
+  for (const item of navItems) {
+    const navItem = document.createElement("li");
+    navItem.innerHTML = `<a class="nav-link" href="#">${item}</a>`;
+    navItem.classList.add("nav-item");
+    console.log(navItem);
+    navContainer.appendChild(navItem);
+  }
+}
+createNavItem(navItems);
+/* ===================== CREATE NAV ITEM END ===================== */
